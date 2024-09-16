8cb6971254a234af0c29b0180e5a40997ba9e31e
Muhammad Zaeem Tauqir <zaeem.tauqir@tekrevol.com>
2024-09-16 07:28:19 -0700

    Add and lists books

--- books/models.py
+++ books/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title 
--- books/views.py
+++ books/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookListCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        books = Book.objects.filter(owner=request.user)
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
